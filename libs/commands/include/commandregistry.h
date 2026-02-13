3c2e25542697ce86596a23c79111a8f2874b4846
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2026-02-13 07:10:54 +0530

    Add CommandRegistry class and implementation files

---
+++ libs/commands/include/commandregistry.h
@@ -0,0 +1,17 @@
+//
+// Created by itsvi on 13-02-2026.
+//
+
+#ifndef LEAF_COMMANDREGISTRY_H
+#define LEAF_COMMANDREGISTRY_H
+
+namespace Leaf
+{
+
+class CommandRegistry
+{
+};
+
+} // namespace Leaf
+
+#endif // LEAF_COMMANDREGISTRY_H
---
+++ libs/commands/src/commandregistry.cpp
@@ -0,0 +1,9 @@
+//
+// Created by itsvi on 13-02-2026.
+//
+
+#include "CommandRegistry.h"
+
+namespace Leaf
+{
+} // namespace Leaf
